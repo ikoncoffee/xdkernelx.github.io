6e37655e6e089190a2412d7b0cc7495b341c8421
Nestor Alvarez <grimreaper1169@gmail.com>
2020-01-02 12:07:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function forceStyle() {
 	widget_frame.id = "rainbow_trial";
 	console.log(widget_frame);
 	console.log(widget_frame.id);
-	var inner_doc = document.getElementById("resizeSub").children[0].children[0].contentWindow;
+	var inner_doc = widget_frame.contentDocument || widget_frame.contentWindow.document;
   console.log(inner_doc);
 }
