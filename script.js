53888fb4f73a3f4f2c32fc9ce9c0d3a4062d46d8
Nestor Alvarez <grimreaper1169@gmail.com>
2020-01-03 22:38:12 -0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function forceStyle() {
-	var widget_frame = document.getElementById("resizeSub").children[0].children[0];
-	widget_frame.id = "rainbow_trial";
-	console.log(widget_frame);
-	console.log(widget_frame.id);
-	var inner_doc = widget_frame.contentDocument || widget_frame.contentWindow.document;
-  console.log(inner_doc);
-}
