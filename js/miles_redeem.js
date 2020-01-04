b8abc058179e8d5455fe6d7351f588a284d2de05
Nestor Alvarez <grimreaper1169@gmail.com>
2020-01-03 16:04:06 -0800

    Update miles_redeem.js

--- js/miles_redeem.js
+++ js/miles_redeem.js
@@ -1,6 +1,1 @@
 
-function checkFrame() {
-	var temp_frame = document.getElementById("code_widget");
-	var innerDoc = temp_frame.contentDocument || temp_frame.contentWindow.document;
-	console.log(innerDoc);
-}
